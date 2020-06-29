32e92b7cc2b204f890bad723a1049710bd325d78
Krisis <czerwinski17@wp.pl>
2020-06-29 15:56:52 +0200

    minor changes

--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -10,21 +10,11 @@ class FeedbackLinearizationController(Controller):
         self.kp = np.diag((1, 1)) * [-2, 1.5]
 
     def calculate_control(self, x, q_d, q_d_dot, q_dd_dot):
-        """
-        Please implement the feedback linearization using self.model (which you have to implement also),
-        robot state x and desired control v.
-        """
-        # q_dot = x[2:, np.newaxis]
-        # # tau = self.model.M(x) * v + self.model.C(x) * q_dot
-        # tau = np.dot(self.model.M(x), v) + np.dot(self.model.C(x), q_dot)
-        #
-        #
-        #
-        # return tau
-        q0_dot = x[2:].reshape(-1,1)
-        q0 = x[:2].reshape(-1,1)
-        q_d_dot = q_d_dot.reshape(-1,1)
-        q_d = q_d.reshape(-1,1)
+
+        q0_dot = x[2:].reshape(-1, 1)
+        q0 = x[:2].reshape(-1, 1)
+        q_d_dot = q_d_dot.reshape(-1, 1)
+        q_d = q_d.reshape(-1, 1)
 
         v = q_dd_dot + self.kd.dot(q0_dot - q_d_dot) + self.kp.dot(q0 - q_d)
 
--- models/manipulator_model.py
+++ models/manipulator_model.py
@@ -5,25 +5,26 @@ class ManiuplatorModel:
     def __init__(self, Tp, m3, r3):
         self.Tp = Tp
         self.l1 = 0.5
-        self.r1 = self.l1/2.
+        self.r1 = 0.01
         self.m1 = 1.
         self.l2 = 0.5
-        self.r2 = self.l2/2.
+        self.r2 = 0.01
         self.m2 = 1.
         self.I_1 = 1 / 12 * self.m1 * (3 * self.r1 ** 2 + self.l1 ** 2)
         self.I_2 = 1 / 12 * self.m2 * (3 * self.r2 ** 2 + self.l2 ** 2)
         self.m3 = m3
         self.r3 = r3
         self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
-        self.alpha = self.I_1 + self.I_2 + self.m1 * self.r1 ** 2 + self.m2 * (self.l1 ** 2 + self.r2 ** 2) +\
-                     self.I_3 + self.m3 * (self.l1 ** 2 + self.l2 ** 2)
-        self.beta = self.m2 * self.l1 * self.r2 + self.m3 * self.l1 * self.l2
-        self.gamma = self.I_2 + self.m2 * self.r2 ** 2 + self.I_3 + self.m3 * self.l2 ** 2
+        self.alpha = self.I_1 + self.I_2 + self.m1 * (self.l1/2.) ** 2 + self.m2 * (self.l1 ** 2 + (self.l2/2.) ** 2) \
+                     + self.I_3 + self.m3 * (self.l1 ** 2 + self.l2 ** 2)
+        self.beta = self.m2 * self.l1 * self.l2/2. + self.m3 * self.l1 * self.l2
+        self.gamma = self.I_2 + self.m2 * (self.l2/2.) ** 2 + self.I_3 + self.m3 * self.l2 ** 2
+
 
     def M(self, x):
         q1, q2, q1_dot, q2_dot = x
 
-        M = np.array([[0,0],[0,0]], float)
+        M = np.array([[0, 0], [0, 0]], float)
         M[0][0] = self.alpha+2*self.beta*np.cos(q2)
         M[0][1] = self.gamma+self.beta*np.cos(q2)
         M[1][0] = self.gamma+self.beta*np.cos(q2)
@@ -34,7 +35,7 @@ class ManiuplatorModel:
     def C(self, x):
         q1, q2, q1_dot, q2_dot = x
 
-        C = np.array([[0,0],[0,0]], float)
+        C = np.array([[0, 0], [0, 0]], float)
         C[0][0] = -self.beta*np.sin(q2)*q2_dot
         C[0][1] = -self.beta*np.sin(q2)*(q1_dot+q2_dot)
         C[1][0] = self.beta*np.sin(q2)*q1_dot
